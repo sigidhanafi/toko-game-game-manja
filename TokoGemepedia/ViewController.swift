0eb13b446bd11bb916de442e996cae027db09deb
sigidhanafi <sigidhanafi@gmail.com>
2019-01-03 13:36:31 +0700

    initial commit

---
+++ TokoGemepedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TokoGemepedia
+//
+//  Created by Nakama on 03/01/19.
+//  Copyright © 2019 TokoGemepedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
